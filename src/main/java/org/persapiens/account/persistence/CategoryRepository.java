46aa171a09329d064ffdd3cff7f60643fb4111ff
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-16 21:58:06 +0000

    feat: adding delete repository methods. related to #10

--- src/main/java/org/persapiens/account/persistence/CategoryRepository.java
+++ src/main/java/org/persapiens/account/persistence/CategoryRepository.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 import org.persapiens.account.domain.Category;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 	Optional<Category> findByDescription(String description);
+
+	@Transactional
+	void deleteByDescription(String description);
 
 }
--- src/main/java/org/persapiens/account/persistence/OwnerEquityAccountInitialValueRepository.java
+++ src/main/java/org/persapiens/account/persistence/OwnerEquityAccountInitialValueRepository.java
@@ -7,9 +7,13 @@ import org.persapiens.account.domain.Owner;
 import org.persapiens.account.domain.OwnerEquityAccountInitialValue;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OwnerEquityAccountInitialValueRepository extends CrudRepository<OwnerEquityAccountInitialValue, Long> {
 
 	Optional<OwnerEquityAccountInitialValue> findByOwnerAndEquityAccount(Owner owner, EquityAccount equityAccount);
+
+	@Transactional
+	void deleteByOwnerAndEquityAccount(Owner owner, EquityAccount equityAccount);
 
 }
