a5ba75849ca2e832ba9f07aeeec5308abacfccb2
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-16 21:42:04 +0000

    feat: cors allowed

--- src/main/java/org/persapiens/account/AccountApplication.java
+++ src/main/java/org/persapiens/account/AccountApplication.java
@@ -1,10 +1,22 @@
 package org.persapiens.account;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication implements WebMvcConfigurer {
+
+	@SuppressFBWarnings("PERMISSIVE_CORS")
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder().sources(AccountApplication.class).run(args);
