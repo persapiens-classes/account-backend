d6e9d6ddc02788af2b2d02bddfb70e06f6192823
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-27 14:36:17 -0300

    fix: mark AccountRepository as primary to avoid conflict with multiple repository implementations.

--- src/main/java/org/persapiens/account/persistence/AccountRepository.java
+++ src/main/java/org/persapiens/account/persistence/AccountRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.persapiens.account.domain.Account;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
+@Primary
 public interface AccountRepository<T extends Account> extends CrudRepository<T, Long>, QueryByExampleExecutor<T> {
 
 	Optional<T> findByDescription(String description);
