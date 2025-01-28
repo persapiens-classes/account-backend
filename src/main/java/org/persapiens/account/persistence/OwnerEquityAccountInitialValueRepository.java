2103fd0513beab851f33e6e95adc99b96e337199
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-28 15:50:22 -0300

    fix: checkstyle

--- src/main/java/org/persapiens/account/persistence/OwnerEquityAccountInitialValueRepository.java
+++ src/main/java/org/persapiens/account/persistence/OwnerEquityAccountInitialValueRepository.java
@@ -6,6 +6,7 @@ import org.persapiens.account.domain.EquityAccount;
 import org.persapiens.account.domain.Owner;
 import org.persapiens.account.domain.OwnerEquityAccountInitialValue;
 import org.persapiens.account.domain.OwnerEquityAccountInitialValueId;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
