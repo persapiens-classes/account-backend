851d58d4f7492809647fe39d27c5aa0d3ee31d9b
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-29 13:42:00 -0300

    chore: Caddyfile is template, receives DOMAIN and configures https. start.sh process Caddyfile.template.

--- start.sh
+++ start.sh
@@ -2,6 +2,15 @@
 
 cd /home/springboot
 
-su - springboot -c "java -Dspring.aot.enabled=true -jar application.jar" &
+echo ${1}
+echo ${2}
+
+export JAVA_OPTS="${2}"
+su - springboot -c "java ${JAVA_OPTS} -Dspring.aot.enabled=true -jar application.jar" &
+
+export DOMAIN="${1}"
+sed "s/{{DOMAIN}}/$DOMAIN/" /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
+
+rm /etc/caddy/Caddyfile.template
 
 caddy run --config /etc/caddy/Caddyfile
