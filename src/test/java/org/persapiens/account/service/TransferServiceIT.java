9b73ffba6ef859c51cae2f56b4cdb51bf0547dd6
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-02-01 23:19:14 +0000

    chore: remove SpringExtension #121

--- src/test/java/org/persapiens/account/service/TransferServiceIT.java
+++ src/test/java/org/persapiens/account/service/TransferServiceIT.java
@@ -3,18 +3,15 @@ package org.persapiens.account.service;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.persapiens.account.AccountApplication;
 import org.persapiens.account.persistence.EquityAccountFactory;
 import org.persapiens.account.persistence.OwnerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = AccountApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class TransferServiceIT {
 
