58385b25ec4210e5de5042df81c6ee83aec91c61
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-02-14 17:36:57 +0000

    feat: adding AttributeNotFoundException to notify conflict bean with not found attributes.

---
+++ src/main/java/org/persapiens/account/service/AttributeNotFoundException.java
@@ -0,0 +1,11 @@
+package org.persapiens.account.service;
+
+public class AttributeNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AttributeNotFoundException(String message) {
+		super(message);
+	}
+
+}
