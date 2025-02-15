504e63c30d37cfb5a6e3f2b53cfa9a30baeffa0d
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-02-15 17:40:07 +0000

    fix: register jjwt impl classes to work in graalvm

---
+++ src/main/java/org/persapiens/account/JJWTNativeConfig.java
@@ -0,0 +1,16 @@
+package org.persapiens.account;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RegisterReflectionForBinding({ io.jsonwebtoken.impl.DefaultJwtParser.class,
+		io.jsonwebtoken.impl.DefaultClaimsBuilder.class,
+		io.jsonwebtoken.impl.security.StandardSecureDigestAlgorithms.class,
+		io.jsonwebtoken.impl.security.StandardKeyOperations.class, io.jsonwebtoken.security.SignatureAlgorithm.class,
+		io.jsonwebtoken.impl.security.StandardEncryptionAlgorithms.class,
+		io.jsonwebtoken.impl.security.StandardKeyAlgorithms.class,
+		io.jsonwebtoken.impl.io.StandardCompressionAlgorithms.class })
+public class JJWTNativeConfig {
+
+}
