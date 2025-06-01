ac3651ef63830a828536b72057779af8f1d0d09f
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-06-01 15:42:09 +0000

    fix: using RegisterReflectionForBinding for dtos register and RuntimeHintsRegistrar for BigDecimal and LocalDateTime register #281

--- src/main/java/org/persapiens/account/DtoNativeConfig.java
+++ src/main/java/org/persapiens/account/DtoNativeConfig.java
@@ -1,11 +1,14 @@
 package org.persapiens.account;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 import org.persapiens.account.dto.AccountDTO;
+import org.persapiens.account.dto.CategoryDTO;
 import org.persapiens.account.dto.EntryDTO;
+import org.persapiens.account.dto.EntryInsertUpdateDTO;
+import org.persapiens.account.dto.ErrorDTO;
+import org.persapiens.account.dto.OwnerDTO;
+import org.persapiens.account.dto.OwnerEquityAccountInitialValueDTO;
 import org.persapiens.account.dto.OwnerEquityAccountInitialValueInsertDTO;
+import org.persapiens.account.dto.OwnerNameEquityAccountDescription;
 
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +16,9 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
 @RegisterReflectionForBinding({
-	// types not automatically registered by spring boot
-	BigDecimal.class, LocalDateTime.class,
 	// dtos not automatically registered by spring boot
-	AccountDTO.class, EntryDTO.class, OwnerEquityAccountInitialValueInsertDTO.class })
+	AccountDTO.class, CategoryDTO.class, EntryDTO.class, EntryInsertUpdateDTO.class, ErrorDTO.class, OwnerDTO.class,
+	OwnerEquityAccountInitialValueDTO.class, OwnerEquityAccountInitialValueInsertDTO.class, OwnerNameEquityAccountDescription.class })
 @ImportRuntimeHints(DtoRuntimeHintsRegistrar.class)
 public class DtoNativeConfig {
 
--- src/main/java/org/persapiens/account/DtoRuntimeHintsRegistrar.java
+++ src/main/java/org/persapiens/account/DtoRuntimeHintsRegistrar.java
@@ -1,11 +1,9 @@
 package org.persapiens.account;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
@@ -14,6 +12,7 @@ public class DtoRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 	/**
 	 * Register constructor and public methods of types BigDecimal and LocalDateTime.
 	 * Dtos use these types.
+	 * These types are not automatically registered by spring boot.
 	 */
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
@@ -22,14 +21,7 @@ public class DtoRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 	}
 
 	private void registerFullReflection(RuntimeHints hints, Class<?> clazz) {
-		// Register all public constructors
-		for (Constructor<?> constructor : clazz.getConstructors()) {
-			hints.reflection().registerConstructor(constructor, ExecutableMode.INVOKE);
-		}
-
-		// Register all public methods
-		for (Method method : clazz.getMethods()) {
-			hints.reflection().registerMethod(method, ExecutableMode.INVOKE);
-		}
+		// Register all public constructors and public methods
+		hints.reflection().registerType(clazz, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS);
 	}
 }
