e4d4e7acad71403232654ab06a468f35ffef92af
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-06-03 11:27:10 +0000

    feat: implement balance endpoints

--- src/main/java/org/persapiens/account/controller/BalanceController.java
+++ src/main/java/org/persapiens/account/controller/BalanceController.java
@@ -1,8 +1,9 @@
 package org.persapiens.account.controller;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
+import org.persapiens.account.dto.BalanceDTO;
 import org.persapiens.account.service.BalanceService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/")
+@RequestMapping("/balances")
 public class BalanceController {
 
 	private BalanceService balanceService;
 
-	@GetMapping("/balance")
-	public BigDecimal balance(@RequestParam(required = true) String owner,
+	@FindOneBeanDoc
+	@GetMapping("/filter")
+	public BalanceDTO balanceByOwnerAndEquityAccount(@RequestParam(required = true) String owner,
 			@RequestParam(required = true) String equityAccount) {
-		return this.balanceService.balance(owner, equityAccount);
+		return this.balanceService.balanceByOwnerAndEquityAccount(owner, equityAccount);
+	}
+
+	@FindAllBeanDoc
+	@GetMapping
+	public List<BalanceDTO> balanceAll() {
+		return this.balanceService.balanceAll();
 	}
 
 }
