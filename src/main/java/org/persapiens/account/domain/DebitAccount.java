d70085fe628347fec20b78762f73b6e7668ac400
Marcelo Romulo Fernandes <persapiens@gmail.com>
2026-08-17 12:29:36 +0000

    chore: upgrade spring javaformat to 0.0.48

--- src/main/java/org/persapiens/account/domain/DebitAccount.java
+++ src/main/java/org/persapiens/account/domain/DebitAccount.java
@@ -10,12 +10,12 @@ import jakarta.persistence.SequenceGenerator;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.jspecify.annotations.NonNull;
 
 @SuppressFBWarnings({ "CT_CONSTRUCTOR_THROW", "NP_NONNULL_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR",
 		"RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE" })
--- src/test/java/org/persapiens/account/IntegrationWebTest.java
+++ src/test/java/org/persapiens/account/IntegrationWebTest.java
@@ -6,11 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(classes = AccountApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/sql/cleanup.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@SpringBootTest(classes = AccountApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/cleanup.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public @interface IntegrationWebTest {
 }
